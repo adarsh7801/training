58a46a2c2b94ae0a22f7421c8f2b045e8ffb3754
adarsh7801 <adarshguptajhs@gmail.com>
2025-01-07 14:11:58 +0530

    Addition program

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class add{
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int a= sc.nextInt();
+		System.out.println("Enter the second number");
+		int b= sc.nextInt();
+		System.out.println("The sum of two numbers is "+a+b);
+		
+}}
